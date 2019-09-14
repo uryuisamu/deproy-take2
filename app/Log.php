de7a3fdbc070306ff93084dfc8baf61ae6869d4f
”名前” <uryuisamu@gmail.com>
2019-09-14 12:05:11 +0000

    Sep.14.sat

--- app/Log.php
+++ app/Log.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Log extends Model
 {
+    protected $guarded = array('id');
+
     public static $rules = array(
         'profile_id' => 'required',
         'edited_at' => 'required',
