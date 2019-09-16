14c65ba1600f0651a747345e333ba9453dc3fd90
”名前” <uryuisamu@gmail.com>
2019-09-16 10:53:45 +0000

    Sep.16.mon

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.front')
+@extends('layouts.profile')
 
 @section('content')
     <div class="container">
@@ -15,45 +15,24 @@
                                     @endif
                                 </div>
                                 <div class="title p-2">
-                                    <h1>{{ str_limit($headline->title, 70) }}</h1>
+                                    <h1>名前・・・{{ str_limit($headline->name, 70) }}</h1>
+                                </div>
+                                <div class="title p-2">
+                                    <h1>性別・・・{{ str_limit($headline->gender, 70) }}</h1>
+                                </div>
+                                <div class="title p-2">
+                                    <h1>趣味・・・{{ str_limit($headline->hobby, 70) }}</h1>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
+                            <p class="body mx-auto">自己紹介⇒{{ str_limit($headline->introduction, 650) }}</p>
                         </div>
                     </div>
                 </div>
             </div>
         @endif
         <hr color="#c0c0c0">
-        <div class="row">
-            <div class="posts col-md-8 mx-auto mt-3">
-                @foreach($posts as $post)
-                    <div class="post">
-                        <div class="row">
-                            <div class="text col-md-6">
-                                <div class="date">
-                                    {{ $post->updated_at->format('Y年m月d日') }}
-                                </div>
-                                <div class="title">
-                                    {{ str_limit($post->title, 150) }}
-                                </div>
-                                <div class="body mt-3">
-                                    {{ str_limit($post->body, 1500) }}
-                                </div>
-                            </div>
-                            <div class="image col-md-6 text-right mt-4">
-                                @if ($post->image_path)
-                                    <img src="{{ asset('storage/image/' . $post->image_path) }}">
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-                    <hr color="#c0c0c0">
-                @endforeach
-            </div>
-        </div>
     </div>
     </div>
 @endsection
